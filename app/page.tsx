ebbcd81522ad627098edf625a9b0229060fdc73c
Glory <gloryope247@gmail.com>
2025-08-13 11:26:25 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Welcome to My Sass App</h1>
+      <Button>Let Get Started</Button>
+    </div>
+  )
+}
+
+export default Page
