a001e714bd0a3a41ebb135cbb59656298ae6c52b
Glory <gloryope247@gmail.com>
2025-08-15 14:05:28 +0100

    added companion

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to My Sass App</h1>
-      <Button>Let Get Started</Button>
-    </div>
+    <main>
+      <h1>Popular Companion </h1>
+      <section className="home-section">
+        <CompanionCard
+        id= "123"
+        name="Neura the Brainy Explorer"
+        topic="Neura Network of the Brain"
+        subject="science"
+        duration={45}
+        color ="#ffda6e"
+         />
+         <CompanionCard
+        id= "456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives and Integrals"
+        subject="math"
+        duration={30}
+        color ="#e5d0ff"
+         />
+          <CompanionCard
+        id= "789"
+        name="Verba the Vocabulary Builder"
+        topic="Language"
+        subject="English Literature"
+        duration={20}
+        color ="#BDE7FF"
+         />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+        title="Recently completed session"
+        companions={recentSessions}
+        classNames = "w-2/3 max-lg:w-full"/>
+        
+        <CTA />
+      </section>
+    </main>
   )
 }
 
