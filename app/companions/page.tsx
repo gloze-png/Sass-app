a719a471c2a0f2223eba079068b27e34018c9915
Glory <gloryope247@gmail.com>
2025-08-20 13:47:23 +0100

    fetch db

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,31 @@
-const CompanionsLibrary = () => {
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject =filters.subject ? filters.subject : '';
+   const topic =filters.topic ? filters.topic : '';
+   const companion = await getAllCompanions({ subject, topic });
+  console.log(companion);
+  
+  // Render the companions library page with the fetched companions
+
   return (
-    <div>
+    <main>
+      <section className=" flex justify-between gap-4 max-sm:flex-col">
+        <h1>Padii Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      <section className="companions-grid">
+        {companion.map((companion) => (
+          <CompanionCard key={companion.id} 
+          {...companion}
+          color= {getSubjectColor(companion.subject)} />
+        ))}
+      </section>
       
-    </div>
+    </main>
   )
 }
 
