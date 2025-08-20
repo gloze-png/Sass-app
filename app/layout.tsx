d8b9931741a9cf038e9a4fdd169de38a0df7e536
Glory <gloryope247@gmail.com>
2025-08-20 04:10:14 +0100

    auth users

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
+import { ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -9,7 +16,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "Padii Learn",
   description: "Real-time AI Teaching Platform",
 };
 
@@ -21,8 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{variables: {colorPrimary: '#fe5933'}} }>
         <Navbar />
-        {children}</body>
+        {children}
+        </ClerkProvider>
+        </body>
     </html>
   );
 }
